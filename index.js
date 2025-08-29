ed269cb6c4187bc22ae85b68e9072f481c772798
abdo-ali <abdelrahmanali102@gmail.com>
2025-08-29 10:35:34 +0300

    add .env and handle it in index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
 import pg from "pg";
+import env from "dotenv";
 
 const app = express();
 const port = 3000;
@@ -12,12 +13,13 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "book",
-  password: "123456",
-  port: 5432,
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
 });
+
 db.connect();
 
 async function fetchItems() {
